d122741be6a79e1fa4248f03294341e834a1b427
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-03-13 18:37:10 +0530

    feature: Attach event listeners to all the images on page

--- extension/content.js
+++ extension/content.js
@@ -1,3 +1,24 @@
 "use strict";
 
-console.log("EXTENSION LOADED");
+const images = document.querySelectorAll("img");
+
+function attachEventListeners(img) {
+    img.addEventListener("mouseenter", function(e) {
+        console.log("MOUSE ENTER");
+        const { x, y, width, height } = img.getBoundingClientRect();
+        console.log(x, y);
+        console.log(width, height);
+    });
+
+    img.addEventListener("mousemove", function(e) {
+        if (!e.ctrlKey) return;
+
+        console.log(e.offsetX, e.offsetY);
+    });
+
+    img.addEventListener("mouseleave", function(e) {
+        console.log("MOUSE LEAVE");
+    });
+}
+
+images.forEach(attachEventListeners);
