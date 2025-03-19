30fabf9e72718ba0a87cce699d7efdd4c101366e
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-03-19 11:48:40 +0530

    feature: Add the styling for magnifier box and result container

--- extension/content.js
+++ extension/content.js
@@ -1,24 +1,18 @@
 "use strict";
 
+console.log("Image Magnifier extension is running.");
+
+// Select all images on the page.
 const images = document.querySelectorAll("img");
 
-function attachEventListeners(img) {
-    img.addEventListener("mouseenter", function(e) {
-        console.log("MOUSE ENTER");
-        const { x, y, width, height } = img.getBoundingClientRect();
-        console.log(x, y);
-        console.log(width, height);
-    });
+// Create the div for magnifier box over the images.
+const magnifier = document.createElement("div");
+magnifier.classList.add("magnifier");
+document.body.append(magnifier);
 
-    img.addEventListener("mousemove", function(e) {
-        if (!e.ctrlKey) return;
-
-        console.log(e.offsetX, e.offsetY);
-    });
-
-    img.addEventListener("mouseleave", function(e) {
-        console.log("MOUSE LEAVE");
-    });
-}
-
-images.forEach(attachEventListeners);
+// Create the container and image to show the result of magnified image.
+const magnifiedResult = document.createElement("div");
+magnifiedResult.classList.add("magnified-result");
+const magnifiedImg = document.createElement("img");
+magnifiedResult.append(magnifiedImg);
+document.body.append(magnifiedResult);
